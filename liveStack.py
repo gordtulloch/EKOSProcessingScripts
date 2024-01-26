d44b5c132221fbeb6fc64ba55c47571672ea0429
Gord Tulloch <gord.tulloch@gmail.com>
2024-01-25 19:02:54 -0600

    Livestack

--- liveStack.py
+++ liveStack.py
@@ -5,6 +5,7 @@
 # Author      : Gord Tulloch
 # Date        : January 25 2024
 # License     : GPL v3
+# Usage       : Called after every image is taken
 # Dependencies: Imagemagick and SIRIL needs to be install for live stacking
 #               Tested with EKOS, don't know if it'll work with other imaging tools 
 # TODO:
@@ -18,6 +19,11 @@ from pysiril.wrapper import *
 picturesFolder="/home/gtulloch/Pictures/"
 workingFolder="/home/gtulloch/SirilWork/"
 
+# Function definitions
+def calibrateImage(imageFile):
+    # Not done yet
+    return imageFile
+
 # Create working directories if not exist
 Path(workingFolder).mkdir(parents=True, exist_ok=True)
 Path(workingFolder+"Light").mkdir(parents=True, exist_ok=True)
@@ -30,35 +36,45 @@ app=Siril()
 cmd=Wrapper(app)
 cmd.set16bits()
 cmd.setext('fits')
+cmd.cd(workingFolder+"/Light")
 
 # Detect any new files in the images folder
-
-# Has a livestack already been started?
-if (os.path.isfile(liveStackName)):
-    # Move the new image into the working folder
-    shutil.copy(fitsName, workingFolder+"Light/Main_002.fits")      
-    # Stack this image with the current liveStack
-    try:
-        cmd.cd(workingFolder+"/Light")
-        cmd.stack("Main_",type='sum',output_norm=False)
-    except Exception as e :
-    print("\n**** ERROR *** " +  str(e) + "\n" )  
-    # Remove working files
-    os.system("rm {0}Light/Main_001.fits".format(workingFolder))
-    os.system("rm {0}Light/Main_002.fits".format(workingFolder))
-    os.system("rm {0}Light/*.seq".format(workingFolder))
-    os.system("mv {0}Light/Main_stacked.fits {0}Light/Main_001.fits".format(workingFolder))
-    os.system("rm {0}Light/r_Main*.fits".format(workingFolder))
-    # Move PNG to web server
-    os.system("/usr/bin/convert -flatten {0}Light/Main_stacked.fits {1}".format(workingFolder,liveStackName))
-else:
-    # New livestack, put the first image in the working folder
-    shutil.copy(fitsName, workingFolder+"Light/Main_001.fits")
-    # Move any existing livestacks to a Previous folder
-    os.system("mv {0}*.png {0}Previous".format(stackFolder))
-    # Create a PNG file of the first image
-    print("Converting {0} to {1}\n".format(fitsName,liveStackName))
-    os.system("/usr/bin/convert -flatten {0} {1}".format(fitsName,liveStackName))
+for root, dirs, files in os.walk(os.path.abspath(picturesFolder)):
+    for file in files:
+        file_name, file_extension = os.path.splitext(os.path.join(root, file))
+        # Ignore everything not a *fit* file
+        if (file_extension !=".fits") or (file_extension !=".fit"):
+            continue
+        # Calibrate the image - choose masters from repository
+        imageFile = calibrateImage(os.path.join(root, file))
+        # Move original file to the raw folder
+        #os.rename(os.path.join(root, file), picturesFolder+"Raw/"+file)
+        # Has a livestack already been started?
+        if (os.path.isfile(liveStackName)):
+            # Move the new image into the working folder
+            shutil.copy(imageFile, workingFolder+"Light/Main_002.fits")      
+            # Stack this image with the current liveStack
+            try:
+                # Stack it with the current master stack
+                cmd.stack("Main_",type='sum',output_norm=False)
+            except Exception as e :
+                print("\n**** ERROR *** " +  str(e) + "\n" )  
+            # Remove working files
+            os.system("rm {0}Light/Main_001.fits".format(workingFolder))
+            os.system("rm {0}Light/Main_002.fits".format(workingFolder))
+            os.system("rm {0}Light/*.seq".format(workingFolder))
+            os.system("mv {0}Light/Main_stacked.fits {0}Light/Main_001.fits".format(workingFolder))
+            os.system("rm {0}Light/r_Main*.fits".format(workingFolder))
+            # Create PNG on web server
+            os.system("/usr/bin/convert -flatten {0}Light/Main_stacked.fits {1}".format(workingFolder,liveStackName))
+        else:
+            # New livestack, put the first image in the working folder
+            shutil.copy(fitsName, workingFolder+"Light/Main_001.fits")
+            # Move any existing livestacks to a Previous folder
+            os.system("mv {0}*.png {0}Previous".format(stackFolder))
+            # Create a PNG file of the first image
+            print("Converting {0} to {1}\n".format(fitsName,liveStackName))
+            os.system("/usr/bin/convert -flatten {0} {1}".format(fitsName,liveStackName))
         
 app.Close()
 del app
